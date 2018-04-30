b510c3dbb9639aa901a78ce6cf5fca071626d70d
Benjamin Geyer <brgeyer49@gmail.com>
2018-04-30 15:27:01 -0400

    percent added

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const budgetController = ( _ => {
     description,
     value
   })
+
+  const calcTotal = type => {
+    data.totals[type] = data.allItems[type].reduce((acc, ele) => acc + ele.value, 0)
+  }
   
   const data = {
     allItems: {
@@ -21,7 +25,9 @@ const budgetController = ( _ => {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   }
 
   return {
@@ -40,6 +46,31 @@ const budgetController = ( _ => {
       data.allItems[type] = [...prevState, newItem ]
     
       return newItem
+    },
+
+    calcBudget: _ => {
+      // calc total income and expenses
+      calcTotal('exp')
+      calcTotal('inc')
+
+      // calc budget
+      data.budget = data.totals.inc - data.totals.exp
+
+      // calc percent
+      if(data.totals.inc > 0){
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100)
+      }else { data.percentage = -1 }
+    },
+
+    getBudget: _ => ({
+      budget: data.budget,
+      totalInc: data.totals.inc,
+      totalExp: data.totals.exp,
+      percentage: data.percentage,
+    }),
+
+    testing: _ => {
+      console.log(data)
     }
   }
 })()
@@ -55,13 +86,17 @@ const UIController = ( _ => {
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
     expenseContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expenseLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   }
 
   return {
     getinput: _ => {
       const type = document.querySelector(DOMstrings.inputType).value // inc or exp
       const desc = document.querySelector(DOMstrings.inputDescription).value
-      const value = document.querySelector(DOMstrings.inputValue).value
+      const value = parseFloat(document.querySelector(DOMstrings.inputValue).value)
   
       return { type, desc, value }
     },
@@ -99,6 +134,12 @@ const UIController = ( _ => {
 
       fieldsArr[0].focus()
     },
+    displayBudget: ({budget, totalInc, totalExp, percentage}) => {
+      document.querySelector(DOMstrings.budgetLabel).textContent = budget
+      document.querySelector(DOMstrings.incomeLabel).textContent = totalInc
+      document.querySelector(DOMstrings.expenseLabel).textContent = totalExp
+      document.querySelector(DOMstrings.percentageLabel).textContent = percentage > 0 ? percentage + '%' : '---'
+    },
 
     DOMstrings: {...DOMstrings},
   }
@@ -118,25 +159,42 @@ const controller = ((budgetCtrl, UICtrl) => {
     })
   }
 
+  const updateBudget = _ => {
+    // calc budget
+    budgetCtrl.calcBudget()
+    // return budget
+    const budget = budgetCtrl.getBudget()
+    // display
+    UICtrl.displayBudget({...budget})
+  }
+
   const ctrlAddItem = _ => {
     // get input 
     const input = UICtrl.getinput()
 
-    // add item to budget
-    const newItem = budgetCtrl.addItem({...input})
-    
-    // add item to ui
-    UICtrl.addListItem(newItem, input.type)
-
-    // clear fields
-    UICtrl.clearFields()
-    // calc budget
-
-    // display
+    if(input.description !== "" && !isNaN(input.value) && input.value > 0){
+      // add item to budget
+      const newItem = budgetCtrl.addItem({...input})
+      
+      // add item to ui
+      UICtrl.addListItem(newItem, input.type)
+  
+      // clear fields
+      UICtrl.clearFields()
+  
+      // calc and update budget 
+      updateBudget()
+    }
   }
   return {
     init: _ => {
       console.log('Application has started')
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1,
+      })
       setupEventListeners()
     }
   }
